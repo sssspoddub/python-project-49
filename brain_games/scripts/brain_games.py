a3b2cb46e9144aa780de60cc19c9b97645f7741b
sssspoddub <spoddub@yahoo.com>
2025-02-25 20:26:00 +0300

    complete third step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 from brain_games.scripts.main import greet
+from brain_games.cli import welcome_user
 
 
 def main():
     greet()
+    welcome_user()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
